97ed32f71fc504ded1ae2434a23e6b324c04f80b
iLynette <lynetteacholah@icloud.com>
2022-09-30 19:45:56 +0300

    Destroy to respond to turbo stream

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -52,6 +52,7 @@ class NotesController < ApplicationController
     @note.destroy
 
     respond_to do |format|
+      format.turbo_stream
       format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
       format.json { head :no_content }
     end
